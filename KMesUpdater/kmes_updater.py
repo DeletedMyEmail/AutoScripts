6adf0ee7724e234f9f6c828cac1da103ae570022
KaitoKunTatsu <88390464+KaitoKunTatsu@users.noreply.github.com>
2022-10-21 10:58:28 +0200

    get repo by name

--- KMesUpdater/kmes_updater.py
+++ KMesUpdater/kmes_updater.py
@@ -4,15 +4,14 @@ from github import Github
 
 
 def listen_for_updates():
-    user = Github(sys.argv[1]).get_user()
-    repo = user.get_repo("KMesRework")
+    repo = Github().get_repo("KMesRework")
     commit_counter = repo.totalCount
-    os.system(f'bash update_kmes.sh {sys.argv[2]} {sys.argv[3]}')
+    os.system(f'bash update_kmes.sh {sys.argv[1]} {sys.argv[2]}')
     while (True)
         sleep(60 - time() % 60)
         if repo.totalCount > commit_counter:
             commit_counter = repo.totalCount
-            os.system(f'bash update_kmes.sh {sys.arg[2]} {sys.arg[3]}')
+            os.system(f'bash update_kmes.sh {sys.arg[1]} {sys.arg[2]}')
 
 if __name__ == '__main__':
     listen_for_updates()
