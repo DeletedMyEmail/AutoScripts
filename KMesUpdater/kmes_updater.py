e112aa9447f3c1caf5325c1e1d28cb8f22060e2e
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-10-23 19:41:32 +0200

    yikes

--- KMesUpdater/kmes_updater.py
+++ KMesUpdater/kmes_updater.py
@@ -8,22 +8,15 @@ from github import Github
 def listen_for_updates():
     g = Github()
     repo = g.get_repo("KaitoKunTatsu/KMesRework")
-    last_jar_name = ""
-    for file in repo.get_files():
-        if file.filename.startswith("KMesServer"):
-            last_jar_name = file.filename
-            break
+    last_commit_counter = len(repo.get_commits())
 
     os.system(f'bash kmes_updater.sh {sys.argv[2]} {sys.argv[3]}')
 
     while (True):
         sleep(60)
-        for file in repo.get_files():
-            if file.filename.startswith("KMesServer"):
-                if file.filename != last_jar_name:
-                    last_jar_name = file.filename
-                    os.system(f'bash kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
-            break
+        if len(repo.get_commits) > last_commit_counter:
+            last_commit_counter = len(repo.get_commits())
+            os.system(f'bash kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
 
 
 if __name__ == '__main__':
