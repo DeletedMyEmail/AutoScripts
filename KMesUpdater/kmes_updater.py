c0e54b15e7b2a61c325f3a6eb34d24adb1843718
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-10-23 18:15:59 +0000

    yikes

--- KMesUpdater/kmes_updater.py
+++ KMesUpdater/kmes_updater.py
@@ -9,14 +9,14 @@ def listen_for_updates():
     g = Github()
     repo = g.get_repo("KaitoKunTatsu/KMesRework")
     last_commit_counter = repo.get_commits().__sizeof__()
-
-    os.system(f'bash kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
+    path_to_this_dir = os.path.dirname(os.path.abspath(__file__))
+    os.system(f'bash {path_to_this_dir}/kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
 
     while (True):
         sleep(60)
         if repo.get_commits().__sizeof__() > last_commit_counter:
             last_commit_counter = repo.get_commits().__sizeof__()
-            os.system(f'bash kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
+            os.system(f'bash {path_to_this_dir}/kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
 
 
 if __name__ == '__main__':
