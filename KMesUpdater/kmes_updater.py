aa374bfae266d91e7d6393b1c7ebbafbbc6ddac5
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-10-25 18:24:59 +0200

    fix commit counter

--- KMesUpdater/kmes_updater.py
+++ KMesUpdater/kmes_updater.py
@@ -7,19 +7,20 @@ from github import Github
 def listen_for_updates():
     g = Github()
     repo = g.get_repo("KaitoKunTatsu/KMesRework")
-    last_commit_counter = repo.get_commits().__sizeof__()
+    last_commit_counter = repo.get_commits().totalCount
     path_to_this_dir = os.path.dirname(os.path.abspath(__file__))
     os.system(f'bash {path_to_this_dir}/kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
 
-    while (True):
+    while True:
         sleep(60)
         repo = g.get_repo("KaitoKunTatsu/KMesRework")
         print("Checking for updates...")
-        if repo.get_commits().__sizeof__() > last_commit_counter:
-            last_commit_counter = repo.get_commits().__sizeof__()
+        if repo.get_commits().totalCount > last_commit_counter:
+            last_commit_counter = repo.get_commits().totalCount
             os.system(f'bash {path_to_this_dir}/kmes_updater.sh {sys.argv[1]} {sys.argv[2]}')
         else:
             print("Already up to date")
+
 
 if __name__ == '__main__':
     listen_for_updates()
--- KMesUpdater/kmes_updater.sh
+++ KMesUpdater/kmes_updater.sh
@@ -2,19 +2,20 @@
 # $1 = path to repo
 # $2 = port
 
-PROCESSTOKILL=$(sudo lsof -t -i:$2)
+PROCESSTOKILL=$(sudo lsof -t -i:"$2")
 if [[ -n "$PROCESSTOKILL" ]]
 then
-	kill -15 "$PROCESSTOKILL"
+	sudo kill -15 "$PROCESSTOKILL"
 	echo "Process killed $PROCESSTOKILL"
 else 
 	echo "KMes not running"
 fi
 
-cd $1
+cd "$1" || exit
 git pull
-cd out/artifacts/KMesReworkServer/
-java -jar *.jar $2 &
+cd out/artifacts/KMesReworkServer/ || exit
+# shellcheck disable=SC2035
+java -jar *.jar "$2" &
 disown -h
 
 echo "KMes Server updated and restarted"
